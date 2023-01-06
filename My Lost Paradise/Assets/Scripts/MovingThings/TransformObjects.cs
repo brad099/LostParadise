81511c33eaea6eabb1a2c6ddd976e3b698f10804
B_r_A_d <brad990124@gmail.com>
2023-01-07 03:43:34 +0400

    Finally the platform started moving correctly

--- My Lost Paradise/Assets/Scripts/MovingThings/TransformObjects.cs
+++ My Lost Paradise/Assets/Scripts/MovingThings/TransformObjects.cs
@@ -5,6 +5,7 @@ public class TransformObjects : MonoBehaviour
 {
 
     [SerializeField] GameObject UpDown;
+    [SerializeField] GameObject Turn1;
     private Rigidbody rb;
     private Rigidbody rbT;
 
@@ -19,8 +20,14 @@ public class TransformObjects : MonoBehaviour
     {
         TurnSpeed = (float)(rb.angularVelocity.y) * 0.1f;
         UpDown.transform.position += new Vector3(0, transform.position.y * TurnSpeed * 0.1f, 0);
-        UpDown.transform.position = new Vector3(UpDown.transform.position.x, Mathf.Clamp(UpDown.transform.position.y, 28, 31), UpDown.transform.position.z);
-        //Turn1.transform.Rotate(new Vector3(0, transform.position.y * TurnSpeed * 5f, 0));
+        //UpDown.transform.position = new Vector3(UpDown.transform.position.x, Mathf.Clamp(UpDown.transform.position.y, 28, 31), UpDown.transform.position.z);
 
+        Turn1.transform.Rotate(new Vector3(0, transform.position.y * TurnSpeed * 5f, 0));
+        // float clampedR = Mathf.Clamp(Turn1.transform.rotation.y, 0f ,180f);
+        //.transform.eulerAngles = new Vector3(Turn1.transform.eulerAngles.x,clampedR,Turn1.transform.eulerAngles.z);
+        // float _rotation = Mathf.Clamp(Turn1.transform.rotation.y, 0, 180);
+        // Turn1.transform.rotation = Quaternion.Euler(Turn1.transform.rotation.x, _rotation, Turn1.transform.rotation.z);
+        //Turn1.transform.eulerAngles = new Vector3(Turn1.transform.eulerAngles.x, Mathf.Clamp(Turn1.transform.eulerAngles.y, 0, 180), Turn1.transform.eulerAngles.z);
+        //.transform.eulerAngles.y = Mathf.Clamp(Turn1.transform.eulerAngles.y, 0, -180);
     }
 }
